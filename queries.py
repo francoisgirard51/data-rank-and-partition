36fd77f7edbbe9c1b67d434de65f4f7ee99c2f01
francois GIRARD <francoisgirard51@gmail.com>
2023-10-27 17:19:41 +0200

    RANK and SUM / OVER

--- queries.py
+++ queries.py
@@ -1,8 +1,33 @@
 # pylint:disable=C0111,C0103
 
 def order_rank_per_customer(db):
-    pass  # YOUR CODE HERE
-
+    query = """
+        SELECT
+            OrderID,
+            CustomerID,
+            OrderDate,
+            RANK() OVER (
+                PARTITION BY CustomerID
+                ORDER BY OrderDate
+            ) AS OrderRank
+        FROM Orders
+        ORDER BY CustomerID, OrderDate
+    """
+    db.execute(query)
+    return db.fetchall()
 
 def order_cumulative_amount_per_customer(db):
-    pass  # YOUR CODE HERE
+    query = """
+        SELECT
+            Orders.OrderID,
+            Orders.CustomerID,
+            Orders.OrderDate,
+            SUM(SUM(OrderDetails.UnitPrice * OrderDetails.Quantity)) OVER(PARTITION BY Orders.CustomerID ORDER BY Orders.OrderDate) OrderCumulativeAmount
+        FROM Orders
+        JOIN OrderDetails ON Orders.OrderID = OrderDetails.OrderID
+        GROUP BY Orders.OrderID
+        ORDER BY Orders.CustomerID
+    """
+    db.execute(query)
+    orders = db.fetchall()
+    return orders
